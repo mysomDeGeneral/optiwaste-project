e2585710a85b26678b775589790f25be584fa1b6
Joshua Quarcoo <joshuaquarcoo97@gmail.com>
2024-05-21 23:56:35 +0000

    commit

--- src/routers/mainrouter.js
+++ src/routers/mainrouter.js
@@ -1,2 +1,36 @@
 const express = require('express');
 let app = express();
+const Router = express.Router();
+app.use(express.static('public'));
+app.set('view-engine','ejs');
+app.use(express.json);
+
+
+
+Router.get('/',(req,res)=>{
+    res.send('signup');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports =Router;
