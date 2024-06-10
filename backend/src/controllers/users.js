61fcd1681f1cb6496f67e1d781a31e9b21ca4106
mysomDeGeneral <fianyekukokonumichael@gmail.com>
2024-06-10 22:37:08 +0000

    Added: user login

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -25,3 +25,27 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: 'Error: ' + error});
     };
 };
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        
+        if(user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                address: user.address,
+                mobile: user.mobile,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password'});
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: 'Error: ' + error});
+    }
+}
