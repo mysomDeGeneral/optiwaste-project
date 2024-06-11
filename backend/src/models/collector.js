f997f4d2a4eab0d76ae7b0929290be5e2a7dfe31
mysomDeGeneral <fianyekukokonumichael@gmail.com>
2024-06-11 23:32:54 +0000

    Added: collector model, controller and route

---
+++ backend/src/models/collector.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const calculateAge = (dob) => {
+    const ageDiffMs = Date.now() - new Date(dob).getTime();
+    const ageDate = new Date(ageDiffMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+};
+
+const MINIMUN_AGE = 24;
+
+
+const collectorSchema = new mongoose.Schema({
+    name : { type: String, required: true },
+    email : { type: String, required: true, unique: true },
+    password : { type: String, required: true },
+    nationalId : { type: String, required: true, unique: true },
+    licenseId : { type: String, required: true, unique: true },
+    dob : { type: Date, required: true , validate: {
+        validator: function(value){
+            return calculateAge(value) >= MINIMUN_AGE;
+        },
+        message: 'Collector must be at least ${MINIMUN_AGE} years old.',
+    },},
+    available: { type: Boolean, required: true, default: true },
+    wasteTypes: [{ type: String, required: true }],
+    location: {
+        latitude: { type: Number, required: true, min: -90, max: 90},
+        longitude: { type: Number, required: true, min: -180, max: 180},
+    }
+}, {
+    timestamps: true,
+});
+
+collectorSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+collectorSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const Collector = mongoose.model('Collector', collectorSchema);
+
+module.exports = Collector;
