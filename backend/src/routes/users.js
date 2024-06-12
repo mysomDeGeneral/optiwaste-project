91294b9fe924d45ab2d2e7c675f68e55829c8d80
mysomDeGeneral <fianyekukokonumichael@gmail.com>
2024-06-12 23:04:29 +0000

    Added: auth middleware, user  profile update

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, logout, getUserProfile } = require('../controllers/users');
+const { register, login, logout, getUserProfile, updateUserProfile } = require('../controllers/users');
+const { protect } = require('../middlewares/auth');
+
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.get('/profile', getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
