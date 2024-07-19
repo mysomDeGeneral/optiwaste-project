9aa92525e6c444cff21c047c94a2b83605ddf5c7
mysomDeGeneral <fianyekukokonumichael@gmail.com>
2024-07-19 16:33:05 +0000

    Suspense to false

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
 };
 
 module.exports = nextConfig;
